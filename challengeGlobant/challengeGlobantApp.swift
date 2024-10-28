c7c9c113414ed7db59e590afeee94cd4ecfd6de9
Christian Santiago Vera Rojas <christian.vera@globant.com>
2024-10-28 16:07:10 -0500

    Initial Commit

---
+++ challengeGlobant/challengeGlobantApp.swift
@@ -0,0 +1,17 @@
+//
+//  challengeGlobantApp.swift
+//  challengeGlobant
+//
+//  Created by Christian Santiago Vera Rojas on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct challengeGlobantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ challengeGlobantTests/challengeGlobantTests.swift
@@ -0,0 +1,17 @@
+//
+//  challengeGlobantTests.swift
+//  challengeGlobantTests
+//
+//  Created by Christian Santiago Vera Rojas on 28/10/24.
+//
+
+import Testing
+@testable import challengeGlobant
+
+struct challengeGlobantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
